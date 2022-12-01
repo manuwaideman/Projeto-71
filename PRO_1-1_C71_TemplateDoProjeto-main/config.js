dc04b9f2277f6aabef274afe2e763280c4db0939
manuwaideman <98489323+manuwaideman@users.noreply.github.com>
2022-12-01 19:21:06 -0300

    Update config.js

--- PRO_1-1_C71_TemplateDoProjeto-main/config.js
+++ PRO_1-1_C71_TemplateDoProjeto-main/config.js
@@ -2,8 +2,12 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 var firebaseConfig = {
-  //Cole sua Configuração do Firebase aqui
-  
+  apiKey: "AIzaSyCPI-l49g9_rzMmY_bXNhDHndVs7hDsM2s",
+  authDomain: "projeto71-2.firebaseapp.com",
+  projectId: "projeto71-2",
+  storageBucket: "projeto71-2.appspot.com",
+  messagingSenderId: "76117639509",
+  appId: "1:76117639509:web:940f0df25339dae59aefdc"
 };
 
 firebase.initializeApp(firebaseConfig);
